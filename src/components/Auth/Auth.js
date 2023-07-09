21049ef260c5744084d4a59d0a1db92185b86b4e
Sasha Anshukov <buenosdias2@yandex.ru>
2023-07-09 22:26:48 +0300

    changed ip, domain address

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,60 +1,60 @@
-export const BASE_URL = 'https://api.jet.nomoredomains.work';
+export const BASE_URL = "https://moviesearch.nomoredomains.work";
 
 // проверка ответа от сервера
 const checkResponse = (response) => {
-    if (response.ok) {
-        return response.json();
-    }
-    return Promise.reject(`Ошибка ${response.status}`);
-}
+  if (response.ok) {
+    return response.json();
+  }
+  return Promise.reject(`Ошибка ${response.status}`);
+};
 
 export const register = (name, email, password) => {
-    return fetch(`${BASE_URL}/signup`, {
-        method: 'POST',
-        headers: {
-        //'Accept': 'application/json',
-        'Content-Type': 'application/json'
-        },
-        credentials: 'include', // теперь куки посылаются вместе с запросом
-        body: JSON.stringify({name, password, email})
-    })
-    .then(checkResponse)
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      //'Accept': 'application/json',
+      "Content-Type": "application/json",
+    },
+    credentials: "include", // теперь куки посылаются вместе с запросом
+    body: JSON.stringify({ name, password, email }),
+  }).then(checkResponse);
 };
 
 // функция, которая будет проверять логин и пароль пользователя
 // на соответствие какому-либо профилю, хранящемуся в базе данных
 export const authorize = (email, password) => {
-    return fetch(`${BASE_URL}/signin`, {
-        method: 'POST',
-        headers: {
-            //'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        credentials: 'include', // теперь куки посылаются вместе с запросом
-        body: JSON.stringify({email, password})
+  return (
+    fetch(`${BASE_URL}/signin`, {
+      method: "POST",
+      headers: {
+        //'Accept': 'application/json',
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // теперь куки посылаются вместе с запросом
+      body: JSON.stringify({ email, password }),
     })
-    .then(checkResponse)
-    /*.then((data) => {
+      .then(checkResponse)
+      /*.then((data) => {
         //console.log(data)
         // сохраняем токен в localStorage
         localStorage.setItem('jwt', data.token);
         return data;
     })*/
-    .catch(err => console.log(err))
+      .catch((err) => console.log(err))
+  );
 };
 
 export const logout = () => {
-    return fetch(`${BASE_URL}/signout`, {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        credentials: "include",
-    })
+  return fetch(`${BASE_URL}/signout`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  })
     .then(checkResponse)
-    .catch(err => console.log(err))
-}
-
+    .catch((err) => console.log(err));
+};
 
 //Запрос для проверки валидности токена и получения email для вставки в шапку сайта
 /*export const tokenCheck  = (token) => {
--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,99 +1,92 @@
 export class MainApi {
-    constructor({adress, apiURL}) {
-        this._adress = adress;
-        this._apiURL = apiURL
-    }
+  constructor({ adress, apiURL }) {
+    this._adress = adress;
+    this._apiURL = apiURL;
+  }
 
-    getUserData() {
-        return fetch(`${this._adress}/users/me`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include'
-        })
-        .then(this._checkResponse)
-    }
+  getUserData() {
+    return fetch(`${this._adress}/users/me`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    }).then(this._checkResponse);
+  }
 
-    setUserData(data) {
-        return fetch(`${this._adress}/users/me`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include',
-            body: JSON.stringify({
-                name: data.name,
-                email: data.email
-            })
-        })
-        .then(this._checkResponse)
-    }
+  setUserData(data) {
+    return fetch(`${this._adress}/users/me`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        name: data.name,
+        email: data.email,
+      }),
+    }).then(this._checkResponse);
+  }
 
-    getSavedMovies() {
-        return fetch(`${this._adress}/movies`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include'
-        })
-        .then(this._checkResponse)
-    }
+  getSavedMovies() {
+    return fetch(`${this._adress}/movies`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    }).then(this._checkResponse);
+  }
 
-    //добавление в избранное
-    createMovie(data) {
-        return fetch(`${this._adress}/movies`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include',
-            body: JSON.stringify({
-                country: data.country,
-                director: data.director,
-                duration: data.duration,
-                year: data.year,
-                description: data.description,
-                image: `${this._apiURL}${data.image.url}`,
-                trailerLink: data.trailerLink,
-                thumbnail: `${this._apiURL}${data.image.url}`,
-                movieId: data.id,
-                nameRU: data.nameRU,
-                nameEN: data.nameEN,
-            }),
-        })
-        .then(res => res.json())
-    }
+  //добавление в избранное
+  createMovie(data) {
+    return fetch(`${this._adress}/movies`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        country: data.country,
+        director: data.director,
+        duration: data.duration,
+        year: data.year,
+        description: data.description,
+        image: `${this._apiURL}${data.image.url}`,
+        trailerLink: data.trailerLink,
+        thumbnail: `${this._apiURL}${data.image.url}`,
+        movieId: data.id,
+        nameRU: data.nameRU,
+        nameEN: data.nameEN,
+      }),
+    }).then((res) => res.json());
+  }
 
-    // удаление из избранного
-    deleteMovie(id) {
-        return fetch(`${this._adress}/movies/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include'
-        })
-        .then(this._checkResponse)
-    }
+  // удаление из избранного
+  deleteMovie(id) {
+    return fetch(`${this._adress}/movies/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    }).then(this._checkResponse);
+  }
 
-    //метод проверки ответа от сервера
-    _checkResponse(response) {
-        // тут проверка ответа
-        if (response.ok) {
-            return response.json();
-        }
-        return Promise.reject(`Ошибка ${response.status}`);
+  //метод проверки ответа от сервера
+  _checkResponse(response) {
+    // тут проверка ответа
+    if (response.ok) {
+      return response.json();
     }
+    return Promise.reject(`Ошибка ${response.status}`);
+  }
 }
 
-
-
 const mainApi = new MainApi({
-    adress: 'https://api.jet.nomoredomains.work', //мой бэк
-    apiURL: 'https://api.nomoreparties.co' // бэк с фильмами
-    //token : '86724e9f-206a-43a9-ab92-a5e8d301d078'
-})
+  adress: "https://movies.nomoredomains.work", //мой бэк
+  apiURL: "https://api.nomoreparties.co", // бэк с фильмами
+  //token : '86724e9f-206a-43a9-ab92-a5e8d301d078'
+});
 
 export default mainApi;
